ea649989e5572e0af2281e219ef61ce3574eb976
liwi6 <156945289+nganhthu@users.noreply.github.com>
2024-03-04 02:21:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-// xử lý tìm kiếm và lọc
-function searchAndFilter() {
-    var searchInput = document.getElementById('searchInput').value.toLowerCase();
-    var statusFilter = document.getElementById('statusFilter').value;
-    
-    // Lấy danh sách kỳ thi
-    var examList = [
-        { name: 'Luyện tập', status: 'accessible' },
-        { name: 'Giữa kỳ', status: 'scheduled' },
-        { name: 'Cuối kỳ', status: 'accessible' },
-    ];
-
-    // Lọc và hiển thị danh sách kỳ thi
-    var filteredExams = examList.filter(function(exam) {
-        return (exam.name.toLowerCase().includes(searchInput) || searchInput === '') &&
-               (statusFilter === 'all' || exam.status === statusFilter);
-    });
-
-    displayExamList(filteredExams);
-}
-
-function displayExamList(exams) {
-    var examListContainer = document.getElementById('examList');
-    examListContainer.innerHTML = '';
-
-    exams.forEach(function(exam) {
-        var listItem = document.createElement('li');
-        listItem.textContent = exam.name + ' - ' + exam.status;
-        examListContainer.appendChild(listItem);
-    });
-}
-
-searchAndFilter();
-
-
-
 //Xử lý logic đăng nhập user, admin
 function validateLoginForm() {
     var username = document.getElementById("username").value;
